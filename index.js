2bb01df7640658ec048ede1f42645b4b970ab4c8
Hayds97 <haydenvander@gmail.com>
2024-03-06 20:49:59 +0000

    added far class for selecting only emojis. Added function to ammend transform value of emoji row to change emoji according to current value in index.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-// selecting the stars in the index.html using class selector. 
+// selecting the stars in the index.html using class selector setting them as constance. 
 const starsEl = document.querySelectorAll(".fa-star"); 
+// selecting emoji elements in the index and setting them as constance. 
+const emojisEl = document.querySelectorAll(".far");
 
 /* selecting array of stars 0-4 using forEach. Adding event(click) listener to each star. called the function updateRating, passing index to function. */
 starsEl.forEach((starEl, index )=>{
@@ -16,5 +18,11 @@ function updateRating(index){
         } else {
             starsEl.classList.remove("active");
         }
-    })
+        
+    });
+    //selecting each emoji. Then amending the transform styling to  X 50px the existing value of transform of the emojis relating to the index value. (0 X-50px = -0px, 1x-50px =-50px and so on.)
+    emojisEl.forEach ((emojiEl)=> {
+    emojiEl.style.transform = `translateX(-${index * 50}px)`; 
+    });
 }
+
